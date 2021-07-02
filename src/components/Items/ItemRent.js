40a6a51f89a11f61a73d480f71a0c0ed8b317bcc
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-07-02 18:41:42 +0200

    contact us

--- src/components/Items/ItemRent.js
+++ src/components/Items/ItemRent.js
@@ -120,7 +120,7 @@ export default function ItemRent(props) {
                             wallet: walletAfter
                         });
 
-                        history.push('profile/renting');
+                        history.push('/profile/renting');
                         console.log(r)})
                     .catch(e=>{
                         console.log(e)
--- src/views/ContactUsPage/Sections/Form.js
+++ src/views/ContactUsPage/Sections/Form.js
@@ -84,8 +84,8 @@ const validationSchema = yup.object().shape({
     .required("Email is required"),
   phone: yup.number("Enter phone").required("phone is required"),
   message: yup
-    .string("Enter store message")
-    .required("store message is required"),
+    .string("Enter message")
+    .required("message is required"),
 });
 const initialValues = {
     name: '',
@@ -101,7 +101,7 @@ export default function Form() {
   const submitForm = (values) => {
     setIsRequesting(true);
 
-    post(`/${modelName}`, values, `${modelName} added successfully!`)
+    post(`/user/contact-us`, values, `${modelName} added successfully!`)
       .then((response) => {
         console.log(response);
       })
@@ -195,9 +195,7 @@ export default function Form() {
             
             />
             <div className={classes.textCenter}>
-              <Button color="primary" round>
-                Contact us
-              </Button>
+              <SubmitButton isRequesting={isRequesting} type="Send Message" />
             </div>
           </form>
         );
