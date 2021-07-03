b0877f1cb56b2f8034df11d1aa7a4ce8a0e5ec6e
Assem Hassan <64113331+AssemALi94@users.noreply.github.com>
2021-07-03 19:32:13 +0200

    adding section to home page

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import RegisterPage from "views/RegisterPage/RegisterPage";
 import UserRoute from "functions/route-guards/UserRoute";
 import VisitorRoute from "functions/route-guards/VisitorRoute";
 import HomePage from "./views/HomePage/HomePage";
+
 import SearchPage from "./views/SearchPage/SearchPage";
 import CategoryPage from "./views/CategoryPage/CategoryPage";
 import SubCategoryPage from "views/SubCategoryPage/SubCategoryPage";
@@ -38,7 +39,7 @@ import Careers from './views/CareersPage/Careers'
 import CartPage from "./views/CartPage/CartPage";
 import FavoritePage from "./views/FavoritePage/FavoritePage";
 import VerifiedUserRoute from "./functions/route-guards/VerifiedUserRoute";
-import {Redirect} from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
@@ -64,9 +65,9 @@ ReactDOM.render(
 
           <UserRoute path="/messenger" component={MessengerPage} />
 
-          
+
           <UserRoute path="/ReferAndEarn" component={ReferAndEarn} />
-          
+
           <UserRoute path="/profile" component={UserProfilePage} />
           <UserRoute path="/user/:id" component={userStorePage} />
 
@@ -92,6 +93,7 @@ ReactDOM.render(
           <Route path="/about-us" component={AboutUsPage} />
 
           <Route exact path="/" component={HomePage} />
+
           <Route component={ErrorPage} />
 
         </Switch>
